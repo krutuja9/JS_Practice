1413fff10c78c0349682401780fdc7065364e922
krutuja9 <krutuja609@gmail.com>
2024-02-06 14:28:46 +0530

    class and Object

---
+++ Class&Object/app.js
@@ -0,0 +1,16 @@
+//try and catch
+
+const  a = 5;
+const  b = 10 ;
+ try{
+  console.log(a+b);
+  console.log(a+b+c);
+  console.log("hello")
+ }catch(err){
+  console.log(err)
+ }
+
+ console.log(a+b);
+ console.log(a+10)
+ console.log(10+10)
+ 
